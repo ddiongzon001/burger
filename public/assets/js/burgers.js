d338abffda2229d8e0bf53cdc052190cc17daf55
Dealan <dealan.diongzon@gmail.com>
2019-08-21 14:46:56 -0400

    changed minor typos

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,3 +1,15 @@
 $(document).on("click", "#submit", function(){
     alert( `this was clicked`);
 })
+
+var createGreeting = function(message, name){
+    return message + ", " + name + "!"
+}
+
+var createGreeting = (message, name) => {
+    return message + ", " + name + "!"
+}
+
+var createGreeting = (message, name) => message + ", " + name + "!";
+
+var greetOne = greetings => console.log(greetings)
