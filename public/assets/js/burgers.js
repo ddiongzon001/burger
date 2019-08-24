9abd628d8a5bd732473a7567dfb7885f083e28d7
Dealan <dealan.diongzon@gmail.com>
2019-08-23 21:37:53 -0400

    finished on click function for the submit button and made the post ajax call

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,9 +1,14 @@
+// On click function for adding new burgers to the page
 $(document).on("click", "#submit", function(){
-    alert( `this was clicked`);
 
-    let newBurger = $(`#usersBurger`).val().trim();
+    // sets the new burgers name into an object
+    let newBurger = {
+        name: $(`#usersBurger`).val().trim()
+    }
 
-    console.log(newBurger);
-
-    // $.post("/api/burgers",)
+    // passes that object into a post AJAX call
+    $.post("/api/burgers", newBurger, function(){
+    })
+    // reloads the page so that the user can see the new burger displayed
+    location.reload();
 })
